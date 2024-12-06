5ab695acc4cc8c1ff86e51a7c345cbd94601df30
madmti <supermatrix9@gmail.com>
2024-12-06 19:30:00 -0300

    Added:  Classes, Schedules models Added: DB scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,12 @@ import vercel from '@astrojs/vercel';
 
 import preact from '@astrojs/preact';
 
+import db from '@astrojs/db';
+
 export default defineConfig({
     site: import.meta.env.PUBLIC_VERCEL_URL,
     trailingSlash: 'always',
-    integrations: [tailwind(), preact()],
+    integrations: [tailwind(), preact(), db()],
     output: 'server',
     adapter: vercel(),
 });
---
+++ db/seed.ts
@@ -0,0 +1,88 @@
+import { Classes, db, Schedules } from 'astro:db';
+
+const user_uid = process.env.PUBLIC_TEST_USER_UID ?? import.meta.env.PUBLIC_TEST_USER_UID;
+
+export default async function seed() {
+	await db.delete(Schedules);
+	await db.delete(Classes);
+
+	await db.insert(Classes).values([
+		{
+			id: 1,
+			user_uid: user_uid,
+			name: 'MAT024'
+		},
+		{
+			id: 2,
+			user_uid: user_uid,
+			name: 'INF155'
+		},
+		{
+			id: 3,
+			user_uid: user_uid,
+			name: 'FIS130'
+		},
+	]);
+	await db.insert(Schedules).values([
+		{
+			class_id: 1,
+			type: 'Catedra',
+			day: 1,
+			place: 'P101',
+			block_mode: true,
+			time: {
+				blocks: ['1-2'],
+			}
+		},
+		{
+			class_id: 1,
+			type: 'Ayudantia',
+			day: 2,
+			place: 'P101',
+			block_mode: true,
+			time: {
+				blocks: ['3-4'],
+			}
+		},
+		{
+			class_id: 2,
+			type: 'Catedra',
+			day: 2,
+			place: 'P101',
+			block_mode: true,
+			time: {
+				blocks: ['1-2'],
+			}
+		},
+		{
+			class_id: 2,
+			type: 'Ayudantia',
+			day: 3,
+			place: 'P101',
+			block_mode: true,
+			time: {
+				blocks: ['3-4'],
+			}
+		},
+		{
+			class_id: 3,
+			type: 'Catedra',
+			day: 3,
+			place: 'P101',
+			block_mode: true,
+			time: {
+				blocks: ['1-2'],
+			}
+		},
+		{
+			class_id: 3,
+			type: 'Ayudantia',
+			day: 4,
+			place: 'P101',
+			block_mode: true,
+			time: {
+				blocks: ['3-4'],
+			}
+		},
+	]);
+}
